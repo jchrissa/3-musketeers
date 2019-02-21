d52ef621b7ebd3c5396810e1b08d83660c991050
jchrissa <46487625+jchrissa@users.noreply.github.com>
2019-02-21 18:53:20 +0100

    "docs(readme): add usage section"

--- cash/README.md
+++ cash/README.md
@@ -1,1 +1,18 @@
 # cash
+### installation
+
+The library cash is used to convert different value from Us dollar to other currency like the british pound sterling (GBP) , euro or Japanese yen(Yen).
+
+# What you need 
+ you need to install first of all in your folder  npm, by taping this command in the cmd : npm install 
+to go to your folder where cash is accesible , you have to go there using this command : cd /path/to/workspace/3-musketeers/cash
+and taping npm install
+
+### Execution 
+
+to execute the program , you have to tap node bin/index.js in the cmd where your file is accessible.
+
+to execute the file : node bin/index.js 
+ if you want to convert a specific amount of money , you can do : node bin/index.js 10 EUR USD
+ this will convert 10 eur to USD .
+ You can see that the green value is the value that has been converted and the grey one is the amount in the original currency. 
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,29 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+     expect(starWars.all).toEqual(          // 1
+  expect.arrayContaining([      // 2
+    expect.stringContaining('')
+  ])
+)
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toEqual(expect.arrayContaining(['Luke Skywalker']))
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toEqual(expect.not.stringContaining('Ben Quadinaros'))
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toEqual(expect.arrayContaining([starWars.random()]))
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.random(4)).toHaveLength(4);
     });
   });
 });
